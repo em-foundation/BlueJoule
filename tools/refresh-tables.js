745557024d60648b4821f018825198d7d9345a11
bob frankel <bob@biosbob.biz>
2025-10-24 15:36:42 -0700

    new score columns

--- tools/refresh-tables.js
+++ tools/refresh-tables.js
@@ -79,15 +79,13 @@ ${genScoreTab('JS220')}
 }
 
 function genScoreTab(aname) {
-    const fill = '&ensp;'
     const pre = `${aname.toLowerCase()}/`
     const pad = aname[0] == 'P' ? '&ensp;&thinsp;' : ''
     const img = '<img src="images/emeralds.svg" width="200" alt="">'
     let res = `<br><a name="${aname.toLowerCase()}-scores"></a><p align="center">${img}</p>
 
-| &emsp;&emsp;${aname} Capture${pad}&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; | ${fill} | &nbsp;00:00:01 · 3V3 &emsp; | &nbsp;00:00:01 · <var>d</var>V<var>d</var> &emsp; | ${fill} | &nbsp;00:00:10 · 3V3 &emsp; | &nbsp;00:00:10 · <var>d</var>V<var>d</var> &emsp; |
-|---|---|---|---|---|---|---|
-|${fill}|${fill}|${fill}|${fill}|${fill}|${fill}|
+| &emsp;&emsp;${aname} Capture${pad}&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; | sleep current (&mu;A) &emsp; | event energy (&mu;J) &emsp; | 1&thinsp;s event period &emsp; | 10&thinsp;s event period &emsp; |
+|---|---|---|---|---|
 `
     for (const [k, v] of CAPS) {
         if (!(k.startsWith(pre))) continue
@@ -104,7 +102,7 @@ function genScoreTab(aname) {
         const x10 = !has_v ? `${ems10}${m10_X}` : ''
         const x1_v = has_v ? `${ems1}${m1}` : ''
         const x10_v = has_v ? `${ems10}${m10}` : ''
-        let line = `| &nbsp;📈&nbsp;${BQ}${cn}${BQ}[&nbsp;&nearr;](../${CAPDIR}/${k}/ABOUT.md#typical-event ${desc}) | ${fill} | &emsp;${x1} | &emsp;${x1_v} | ${fill} | &emsp;${x10} | &emsp;${x10_v} |`
+        let line = `| &nbsp;📈&nbsp;${BQ}${cn}${BQ}[&nbsp;&nearr;](../${CAPDIR}/${k}/ABOUT.md#typical-event ${desc}) | &emsp; | &emsp; | &emsp;${x1_v} | &emsp;${x10_v} |`
         getEmeralds(v)
         res += `${line}\n`
     }
