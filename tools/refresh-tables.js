77eabca24aadb24b2bd7e7efbb369e8dc60f0aea
bob frankel <bob@biosbob.biz>
2025-10-28 16:17:23 -0700

    WIP

--- tools/refresh-tables.js
+++ tools/refresh-tables.js
@@ -141,8 +141,8 @@ function genScoreTab(aname) {
     }
     res += `
     
-| Capture | sleep current [&thinsp;&mu;A&thinsp;] | event energy [&thinsp;&mu;J&thinsp;] | 1&thinsp;s period [${EMS}] | 10&thinsp;s period [${EMS}] |
-|:---:|:---:|:---:|:---:|:---:|
+| &emsp;&emsp;Capture | sleep current [&thinsp;&mu;A&thinsp;] | event energy [&thinsp;&mu;J&thinsp;] | 1&thinsp;s period [${EMS}] | 10&thinsp;s period [${EMS}] |
+|---|:---:|:---:|:---:|:---:|
 `
     for (const [k, v] of CAPS) {
         if (aname == 'Entry') {
